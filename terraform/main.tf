3a90999536ac8a25d430cb72e9bdda0b17e286d8
sivakethineni <37953891+sivakethineni@users.noreply.github.com>
2022-03-04 06:41:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,8 @@ resource "aws_instance" "Docker-Server" {
     ami = "ami-08ee6644906ff4d6c" 
     instance_type = "t2.micro" 
     key_name = "Jenkins-new-key"
-    security_groups = ["All-Traffic-SG"] 
+    security_groups = ["All-Traffic-SG"]
+    user_data = file("entry-script.sh")
     tags = {
         Name = "Docker Server by Terraform"
     }
