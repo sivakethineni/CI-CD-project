bae5a010d9899fbe5ee0bd8f19a6d9457868f852
sivakethineni <37953891+sivakethineni@users.noreply.github.com>
2022-03-04 07:28:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+    required_version = ">= 0.12"
+    backend "s3" {
+        bucket = "cicdprojecttesting123"
+        key = "myapp/state.tfstate"
+        region = "ap-south-1"
+    }
+}
+
 provider "aws" {
     region = "ap-south-1"
 }
