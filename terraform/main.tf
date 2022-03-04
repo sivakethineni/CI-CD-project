95f8c91564377bce59bdbe6c56dedf54734af425
sivakethineni <37953891+sivakethineni@users.noreply.github.com>
2022-03-04 07:19:47 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,21 @@ provider "aws" {
     region = "ap-south-1"
 }
 
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
 resource "aws_instance" "Docker-Server" {
-    ami = "ami-076754bea03bde973" 
+    ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = "t2.micro" 
     key_name = "Jenkins-new-key"
     security_groups = ["All-Traffic-SG"]
