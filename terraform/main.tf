c52c212e74301c7d366c9f6e2a5d470ef7b83353
sivakethineni <37953891+sivakethineni@users.noreply.github.com>
2022-03-04 07:24:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,10 @@ data "aws_ami" "latest-amazon-linux-image" {
     }
 }
 
+output "ami_id" {
+  value = data.aws_ami.amazon-linux-image.id
+}
+
 resource "aws_instance" "Docker-Server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = "t2.micro" 
@@ -25,6 +29,7 @@ resource "aws_instance" "Docker-Server" {
         Name = "Docker Server by Terraform"
     }
 }
+
 
 output "ec2_public_ip" {
     value = aws_instance.Docker-Server.public_ip
